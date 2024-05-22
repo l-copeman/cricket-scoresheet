90368b96d73cdca53460c1984fdafabd08e9ec17
Lee Copeman <leepcopeman@gmail.com>
2024-05-22 16:11:32 +0000

    Add a for loop to the get_score_data function to run 20 times, for the innings of the team.

--- run.py
+++ run.py
@@ -31,19 +31,19 @@ def get_scores_data():
     if a player is out. The loop will repeatedly request data, 
     until it is valid. 
     """
-    while True:
-        print("Please enter the scores from the last over.")
-        print("Scores can be 0-6 or 'W' if a player is out." )
-        print('Enter 6 scores for the over, each seperated by a comma.')
-        print("Example: 0,2,1,4,W,0\n")
+    print("Please enter the scores, one over at a time.")
+    print("Scores can be 0-6 or 'W' if a player is out." )
+    print('Enter 6 scores for the over, each seperated by a comma.')
+    print("Example: 0,2,1,4,W,0\n")
+    for i in range(20):
+        while True:
+            score_str = input(f"Enter the score from the over {i + 1}/20:\n")
+            score_over = score_str.split(",")
 
-        score_str = input("Enter the score from the over:\n")
-
-        score_over = score_str.split(",")
-
-        if validate_data(score_over):
-            print("Data is valid!")
-            break
+            if validate_data(score_over):
+                print("Data is valid!\n")
+                update_scoresheet(score_over, 'Team A')
+                break  
 
     return score_over
 
@@ -60,7 +60,7 @@ def validate_data(data):
 
         # Check each character in the input string
         for input in data:
-            if input not in '0123456wW':
+            if input not in '0,1,2,3,4,5,6,w,W':
                 raise ValueError(f"Invalid character found: {input}")
 
         # If all checks pass, the input is valid
@@ -81,13 +81,12 @@ def update_scoresheet(data, worksheet):
     worksheet_to_update.append_row(data)
     print('Scoresheet updated successfully\n')
 
-
 def main():
     """
     Run all program functions
     """
     enter_team_names()
-    score_over = get_scores_data()
+    score = get_scores_data()
     update_scoresheet(score_over, 'Team A')
 
 
